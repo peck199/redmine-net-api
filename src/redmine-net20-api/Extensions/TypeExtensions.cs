00314dea1ef14a260cd43794ad49f642a5a2b03d
Zapadi <adi.popescu@gmail.com>
2019-07-27 15:33:42 +0300

    Add TypeExtensions.

---
+++ src/redmine-net20-api/Extensions/TypeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RedmineClient.Extensions
+{
+    /// <summary>
+    /// /
+    /// </summary>
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TInterface"></typeparam>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static bool Implements<TInterface>(this Type type) where TInterface : class
+        {
+            var interfaceType = typeof(TInterface);
+
+            if (!interfaceType.IsInterface)
+            {
+                throw new InvalidOperationException("Only interfaces can be implemented.");
+            }
+
+            return (interfaceType.IsAssignableFrom(type));
+        }
+    }
+}
